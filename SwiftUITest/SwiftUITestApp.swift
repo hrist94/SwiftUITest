49f5fa84782b2038f55e23f37c9db07e63ca28aa
k.khristenko <k.v.hrist@gmail.com>
2022-10-18 15:59:40 +0300

    Initial Commit

---
+++ SwiftUITest/SwiftUITestApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUITestApp.swift
+//  SwiftUITest
+//
+//  Created by Kirill Khristenko on 18.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUITestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
